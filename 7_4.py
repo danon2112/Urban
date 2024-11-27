4133e46a238001b199ad3cf9cf83717b0cd45ee3
danon2112 <d.ivanov.d2112@gmail.com>
2024-11-28 00:14:18 +0300

    Upload 7_4.py

---
+++ 7_4.py
@@ -0,0 +1,27 @@
+from contextlib import nullcontext
+from time import process_time_ns
+
+team_num1 = 5
+print('В команде Мастера кода участников: %s ! ' % team_num1)
+
+team_num1 = 5
+team_num2 = 6
+print('В команде Мастера кода участников: %s и %s ! ' % (team_num1, team_num2))
+
+score_1 = 42
+print('Команда Волшебники данных решила задач: {} !'.format(score_1))
+
+team1_time = 18015.2
+print('Волшебники данных решили задачи за {} с !'.format(team1_time))
+
+score_1 = 40
+score_2 = 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+challenge_result = 'победа команды Мастера кода!'
+print(f'Результат битвы: {challenge_result}')
+
+task_total = 82
+time_avg = 350.4
+print(f'Сегодня было решено {task_total} задач, в среднем по {time_avg} секунды на задачу!.')
+
