771442ce9da285fa5f27a2e4ca20b06f95fcb789
danon2112 <d.ivanov.d2112@gmail.com>
2024-11-30 02:11:43 +0300

    Fix 7_5.py

--- 7_5.py
+++ 7_5.py
@@ -4,8 +4,8 @@ import os
 directory = '.'
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(file)
-        filetime = time.ctime(os.path.getmtime(file))
-        filesize = os.path.getsize(file)
+        filepath = os.path.join(root, file)
+        filetime = time.ctime(os.path.getmtime(filepath))
+        filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(file)
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {filetime}, Родительская директория: {parent_dir}')
