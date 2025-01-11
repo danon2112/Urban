7af1584971ce01f42f9ee0d9c3b405d070245bb8
danon2112 <d.ivanov.d2112@gmail.com>
2025-01-11 16:31:34 +0300

    Update 10_2: fix bugs

--- 10_2.py
+++ 10_2.py
@@ -28,6 +28,9 @@ class Knight(threading.Thread):
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
-first_knight.run()
-second_knight.run()
+first_knight.start()
+second_knight.start()
 
+first_knight.join()
+second_knight.join()
+print('Битвы завершены')
